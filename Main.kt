425d0276265e9e9951ace7017d2c1dddd5216d7d
Deargdue1 <34446462+Deargdue1@users.noreply.github.com>
2017-12-11 20:22:29 +0200

    Delete Main.kt

--- Main.kt
+++
@@ -1,6 +0,0 @@
-fun main(args: Array<String>) {
-    val playbutton = Gamemode()
-    println("Добро пожаловать в игру \"Кто хочет стать Миллионером!?\"")
-    println("Ответьте правильно на все вопросы и получите милион тугриков!")
-    playbutton.start()
-}
